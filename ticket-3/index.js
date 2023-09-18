16261112089910242a9a08de198e2d87431da4ca
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2023-09-18 16:19:19 +0100

    ticket 4

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,12 +1,20 @@
 function countNumberOfNinesInNumber(number) {
   let count = 0;
 
-  for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
-    if ("9" === digit) {
+  let numberSpr = number.toString ();
+
+  for (let i = 0; i < numberSpr.length; i++) {
+    const digit = numberSpr[i];
+    if (digit == "9") {
       count++;
     }
-  }
 
-  return count;
+  //return count;
 }
+return count;
+}
+
+//countNumberOfNinesInNumber(9)         
+//countNumberOfNinesInNumber(989)       
+//countNumberOfNinesInNumber(1115.1257846) 
+//How could you check if the for loop is running and how many iterations it completes before ending?
